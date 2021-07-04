7ce8f15f35d8e42a7dcde8c1c99ebf616f8ce6b7
dezfuture <agrim2000@gmail.com>
2021-07-04 15:32:59 +0530

    Handled the post appearance and the grid layout

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
@@ -10,12 +11,21 @@ function Posts() {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
 
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="strech"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
